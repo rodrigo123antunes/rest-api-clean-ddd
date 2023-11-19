9312dd3f3758702bf1fb091d55a9cfddde197e1b
“Rodrigo <rodrigobpzantunes2016@gmail.com>
2023-11-18 21:43:04 -0300

    refactor: base class of entities

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,21 +1,13 @@
-import { randomUUID } from "node:crypto"
+import { Slug } from "./value-objects/slug"
+import { Entity } from "../../core/entities/entity"
 
 interface QuestionProps {
   title: string
   content: string
+  slug: Slug
   authorId: string
 }
 
-export class Question {
-  public id: string
-  public title: string
-  public content: string
-  public authorId: string
+export class Question extends Entity<QuestionProps> {
 
-  constructor(props: QuestionProps, id?: string) {
-    this.title = props.title
-    this.content = props.content
-    this.authorId = props.authorId
-    this.id = id ?? randomUUID()
-  }
 }
--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -1,11 +1,7 @@
-import { randomUUID } from "node:crypto"
+import { Entity } from "../../core/entities/entity"
+interface StudentProps {
+  name: string
+}
+export class Student extends Entity<StudentProps> {
 
-export class Student {
-  public id: string
-  public name: string
-
-  constructor(name: string, id?: string) {
-    this.name = name
-    this.id = id ?? randomUUID()
-  }
 }
