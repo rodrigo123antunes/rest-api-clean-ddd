657fdd2beb935d83f935c8d354f8ab33a586a2d9
“Rodrigo <rodrigobpzantunes2016@gmail.com>
2023-11-21 21:17:16 -0300

    refactor: all

--- src/core/entities/unique-entity-id.ts
+++ src/core/entities/unique-entity-id.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from "node:crypto"
+import { randomUUID } from 'node:crypto'
 
 export class UniqueEntityID {
   private value: string
@@ -13,5 +13,9 @@ export class UniqueEntityID {
 
   constructor(value?: string) {
     this.value = value ?? randomUUID()
+  }
+
+  public equals(id: UniqueEntityID) {
+    return id.toValue() === this.value
   }
 }
